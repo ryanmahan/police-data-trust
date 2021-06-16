995067b4b80c8ae223ecf663b2b1fb30b94e35a2
Ryan Mahan <ryanmahan97@gmail.com>
2021-06-15 20:33:12 -0400

    test prettier

---
+++ frontend/.next/static/_PqrLK968qeFrpWBy-Suo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-5c68ead3a3b9912af5c2.js"],"/_error":["static/chunks/pages/_error-b8e575aa54a3bcf36eff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ frontend/.next/static/uS7ylLQiqBjwGvjpleVVT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-3cdf699bc0f66d306baa.js"],"/_error":["static/chunks/pages/_error-b8e575aa54a3bcf36eff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
