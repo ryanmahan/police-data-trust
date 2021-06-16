16124919a8c56da02cf77f6ab3d3adc384b6bcf4
Ryan Mahan <ryanmahan97@gmail.com>
2021-06-15 21:00:27 -0400

    test

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div>I am the Police Data Trust Application! Test Prettier</div>
+  return <div>I am the Poli
+    
+    
+    ce Data Trust Application! Test Prettier</div>
 }
+  
